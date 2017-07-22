454e8573783dac7e883f61598a33fe66b9f288df
huangkangqiang <243286916@qq.com>
2017-07-22 22:47:01 +0800

    update

--- extensions/octref.vetur-0.8.6/build/update-docs.sh
+++
@@ -1,9 +0,0 @@
-cd docs
-rm -rf _book
-gitbook install
-gitbook build
-cd _book
-git init
-git add -A
-git commit -m 'update book'
-git push -f git@github.com:octref/vetur.git master:gh-pages
--- extensions/octref.vetur-0.8.6/docs/roadmap.md
+++
@@ -1,3 +0,0 @@
-# Roadmap
-
-See https://github.com/octref/vetur/issues/188 for now.
